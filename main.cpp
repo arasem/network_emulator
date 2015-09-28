6d093961b6a4a606a41a8c50b5bf98c6a466bb97
arasem <emekcanaras@gmail.com>
2015-09-29 00:50:57 +0200

    main function changed according to recent architecture. nodes can be created and connection between them can be established now. However simulation behaviour cannot run yet just connected nodes can be tested now.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,117 @@
 
-
+#include "conf.h"
 #include "iostream"
+#include  "pthread.h"
+#include "stdlib.h"
+#include "elements.h"
+#include "sim.h"
+#include <sys/resource.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+static void event_cb(port *port_p ){
+    //int *value = (int*)(data);
 
-typedef void (*rectangle_cb_t)( void *data);
 
-class Rectangle {
-    int width, height;
+std::cout<<"thread1s input number :" << port_p->input_number << "\n"<< std::endl;
+    std::cout<<"thread1s problem :" << port_p->input_p[0]->problem << "\n"<< std::endl;
 
-  public:
-    rectangle_cb_t cb;
-
-};
-
-static void event_cb( void *data){
-
-    std::cout << "callback \n" ;
+    while(1);
 
 }
 
-int main(){
-    int value = 5;
-    void *ptr = &value;
+static void event_cb2(port *port_p ){
+    //int *value = (int*)(data);
 
-    Rectangle Rec1;
-    Rec1.cb = event_cb;
-    Rec1.cb(ptr);
+    std::cout<<"thread2s input number :" << port_p->input_number << "\n"<< std::endl;
+    std::cout<<"thread2s problem :" << port_p->input_p[0]->problem << "\n"<< std::endl;
+    while(1);
 
-    std::cout << "Hello \n";
+}
+
+
+int main(int argc, char *argv[]){
+  /*  int stack_size,opt;
+    void *res;
+ const rlim_t kStackSize = 256L * 1024L * 1024L;   // min stack size = 64 Mb
+    struct rlimit rl;
+rl.rlim_cur = kStackSize;
+setrlimit(RLIMIT_STACK, &rl);
+    stack_size = -1;
+    while ((opt = getopt(argc, argv, "s:")) != -1) {
+       switch (opt) {
+       case 's':
+           stack_size = strtoul(optarg, NULL, 0);
+           break;
+
+       default:
+           fprintf(stderr, "Usage: %s [-s stack-size] arg...\n",
+                   argv[0]);
+           exit(EXIT_FAILURE);
+       }
+    }
+*/
+    int value = DUMMY;
+
+    node **node_array;
+    port *port_node0 = new port;
+    port *port_node1 = new port;
+    connection **input_array_node0;
+    connection **input_array_node1;
+    connection **output_array_node0;
+    connection **output_array_node1;
+
+    node_array = new node *[2];
+
+
+
+    input_array_node0 = new connection *[1];
+    input_array_node1 = new connection *[1];
+    output_array_node0 = new connection *[1];
+    output_array_node1 = new connection *[1];
+
+    input_array_node0[0] =  new connection;
+    input_array_node0[0]->problem = 1;
+
+    input_array_node1[0] =  new connection;
+    input_array_node1[0]->problem = 0;
+
+    port_node0->input_number = 1;
+    port_node0->input_p = input_array_node0;
+    port_node0->output_number = 1;
+    port_node0->output_p = output_array_node0;
+
+    port_node1->input_number = 1;
+    port_node1->input_p = input_array_node1;
+    port_node1->output_number = 1;
+    port_node1->output_p = output_array_node1;
+
+    node_array[0] = new node;
+    node_array[0]->cb = event_cb;
+    node_array[0]->problem = 1;
+    node_array[0]->port_p = port_node0;
+    node_array[1] = new node;
+    node_array[1]->cb = event_cb2;
+    node_array[1]->problem = 1;
+    node_array[1]->port_p = port_node1;
+
+
+
+    network *network1 = new network;
+    std::cout << "Hello \n" << std::endl;
+
+
+    network1->node_number = 2;
+    network1->node_p = node_array;
+    sim sim1;
+
+    sim1.create(network1);
+    sim1.run(network1);
+
+    while(1);
 
 return 0;
 
