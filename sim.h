5990ea9b9ae89cef0778de86f464b977ac79c08b
arasem <emekcanaras@gmail.com>
2015-10-08 15:59:12 +0200

    sim constructor has been changed. event_list has been added in order to execute discrete event simulation

--- sim.h
+++ sim.h
@@ -4,16 +4,27 @@
 #include "elements.h"
 #include "pthread.h"
 
+
+
 class sim{
     private:
-        pthread_t thread;
+        int current_sim_time = 0;
+        int event_number;
     public:
-        sim();
+       int ms_sim_time;
+        int ms_sim_resolution;
+        sim(int , int  );
         //bool create(node *node_p);
         bool create(network *network_p);
         bool run(network *network_p);
 };
 
+class event_list{
+    public:
+    int ms_event_time;
+    int event_array[50];
+    int event_number;
+};
 
 
 #endif
