7780867719ba42b3a316e615ccfa41fb8c6f2b34
arasem <emekcanaras@gmail.com>
2015-10-18 19:44:39 +0200

    emptystep parameter has been added to event_list class in order to fix segmantation fault problem

--- sim.h
+++ sim.h
@@ -5,6 +5,11 @@
 #include "pthread.h"
 
 
+typedef enum{
+    EXPONENTIAL_DIST,
+    WEIBULL_DIST
+}distribution_option_t;
+
 
 class sim{
     private:
@@ -12,12 +17,13 @@ class sim{
         int event_number;
         bool *prob;
     public:
-       int ms_sim_time;
+        int ms_sim_time;
         int ms_sim_resolution;
-        sim(int , int  );
+        distribution_option_t distribution_t;
         //bool create(node *node_p);
         bool create(network *network_p);
         bool run(network *network_p);
+        sim(int , int  );
 };
 
 class event_list{
@@ -25,6 +31,7 @@ class event_list{
     int ms_event_time;
     int event_array[50];
     int event_number;
+    bool empty_step;
 };
 
 
