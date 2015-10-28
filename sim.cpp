468ff2cfb138be60d4332b16842c757e0d99b0de
arasem <emekcanaras@gmail.com>
2015-10-28 13:12:40 +0100

    some bugs have been fixed and scalebility parameters has been added in order to scale the probability function

--- sim.cpp
+++ sim.cpp
@@ -25,13 +25,14 @@ bool sim_not_finished = 1;
 bool regular_thread_finished = 0;
 bool periodic_thread_finished = 0;
 int regular_engine_event_counter = 0;
+bool *global_prob;
 
 sim::sim(int ms_time, int ms_resolution){
     int dummy;
 
     ms_sim_time = ms_time;
     ms_sim_resolution = ms_resolution;
-
+    ms_resolution_global = ms_resolution;
 
     dummy = (int) (ms_sim_time / ms_sim_resolution);
     event_number = dummy;
@@ -47,7 +48,7 @@ void *thread_func(void *node_p){
 
     node_ptr->behaviour(node_ptr->port_p);
 
-   pthread_exit(0);
+    pthread_exit(0);
 
    }
 
@@ -87,13 +88,19 @@ void *periodic_engine(void *params){
     network *p_network = new network;
     p_network = (network *)params;
     int node_counter = 0;
+    int problem_number;
 
     while(sim_not_finished){
     if(periodic_thread_finished == 0){
         for(node_counter = 0; node_counter < p_network->node_number; node_counter++){
             if(p_network->node_p[node_counter]->continious){
                 if(p_network->node_p[node_counter]->ms_interaction_time < current_time){
-                        p_network->node_p[node_counter]->behaviour(p_network->node_p[node_counter]->port_p);
+                        if(global_prob[((p_network->node_p[node_counter]->ms_interaction_time )/ ms_resolution_global) -1])
+                        {
+                            std::cout<<"Problem occured at Simulation time" << current_time << "ms \n"<< std::endl;
+                        }else{
+                            p_network->node_p[node_counter]->behaviour(p_network->node_p[node_counter]->port_p);
+                        }
                 }
             }
         }
@@ -124,15 +131,18 @@ bool nextBool(double probability)
 }
 
 bool sim::create(network *network_p){
-
+    int scale;
     const int nrolls = 10000;  // number of experiments
     int nintervals; // number of intervals
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     float lambda;
     float alfa;
-    bool dummy[9];
-    double dummy2[9];
+    bool dummy[this->event_number];
+    double dummy2[this->event_number];
+    global_prob = (bool *) malloc(this->event_number + 1);
+
+    scale = this->event_number / 20;
 
     switch(this->distribution_t){
         case EXPONENTIAL_DIST:
@@ -142,9 +152,9 @@ bool sim::create(network *network_p){
             //nrolls = 10000;
         break;
         case WEIBULL_DIST:
-            alfa= 2.0;
-            lambda = 4.0;
-            nintervals= 1;
+            alfa= 1;//shape parameter
+            lambda = 0.2;//1/lambda = failure rate
+            nintervals= this->event_number;
             //nrolls = 1000;
         break;
         default:
@@ -157,19 +167,19 @@ bool sim::create(network *network_p){
 
     int* p= new int[this->event_number];
     double *prob = new double[this->event_number];
-    bool *real_prob = new bool[this->event_number];
+    bool *real_prob = new bool[this->event_number + 1];
 
     for (int i=0; i<nrolls; ++i) {
     double number = distribution(generator);
-     ++p[int(nintervals*number)];
+    if (number<1.0)  ++p[int(nintervals*number)];
     }
-    for (int i=0; i<this->event_number; ++i) {
-        prob[i]= double(p[i])/double(nrolls);
+    for (int i=0; i<this->event_number + 1; ++i) {
+        prob[i]= double(p[i]*scale)/double(nrolls);
         dummy2[i] = prob[i];
         real_prob[i] = nextBool(prob[i]);
         dummy[i] = real_prob[i] ;
     }
-
+    global_prob = real_prob;
     this->prob = real_prob;
 
     return 0;
